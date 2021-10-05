a47b89326eeaefa48fc039684e9344462097d97d
AlaaAEid <alaa_eid1@outlook.com>
2021-10-05 19:54:40 +0300

    Day 5

--- src/CodeForces/A131CapsLock.java
+++ src/CodeForces/A131CapsLock.java
@@ -6,7 +6,7 @@ public class A131CapsLock {
     public static void main (String [] args) {
         Scanner scan = new Scanner(System.in);
         String input = scan.next();
-        if (input.matches("^[A-Z]$") || input.matches("^[a-z]{1}[A-Z]$") ){
+        if (input.matches("^[A-Z]*$") || input.matches("^[a-z]{1}[A-Z]{1,}$") ){
             input = input.toLowerCase();
             System.out.println((char)(input.charAt(0)-32) + input.substring(1));
         }
